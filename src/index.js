f59cd5d2f2cef401d836ad0c53e61190fe3fc005
Lukylix <lucas.garcia.bobi@gmail.com>
2022-05-28 20:27:02 +0200

    React Router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import reportWebVitals from "./reportWebVitals";
+
+import Home from "./screens/Home";
+
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
